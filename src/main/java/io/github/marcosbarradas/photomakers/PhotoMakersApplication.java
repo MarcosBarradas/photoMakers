9f306f354655a7a266328f3644fc2393932f2734
MarcosBarradas <vini.moraes.1518@email.com>
2025-08-02 11:36:40 -0300

    test

--- src/main/java/io/github/marcosbarradas/photomakers/PhotoMakersApplication.java
+++ src/main/java/io/github/marcosbarradas/photomakers/PhotoMakersApplication.java
@@ -2,10 +2,9 @@ package io.github.marcosbarradas.photomakers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//changeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 @SpringBootApplication
 public class PhotoMakersApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PhotoMakersApplication.class, args);
     }
